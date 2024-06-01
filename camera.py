3fea3a2fbd92a40d46a26f3827057f66b832e691
iftachshalev <iftachshalev30115@gmail.com>
2024-06-01 20:07:05 +0300

    adapting camera for rpi 5

--- camera.py
+++ camera.py
@@ -4,15 +4,13 @@ import cv2
 class Camera:
     
     def __init__(self):
-        """
-        init the cam
-        """
-        pass
+        self.cam = cv2.VideoCapture(0)
     
     def read_img(self):  # will take a pic on the rpi cam
-        img = cv2.imread('data/for_now.jpg')
-        # cv2.imshow('image', img)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+        # img = cv2.imread('data/for_now.jpg')
+        ret, img = self.cam.read()
+        cv2.imshow('image', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         return img
 
