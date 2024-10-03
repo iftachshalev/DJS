506500c05871e99d53c39e1154b7bfec85aaa60d
iftachshalev <iftachshalev30115@gmail.com>
2024-10-03 12:37:20 +0300

    fix

--- DJS-sim/simulation/simulation.py
+++ DJS-sim/simulation/simulation.py
@@ -1,5 +1,4 @@
 from junction import *
-import logging as log
 
 
 def check_junction():
@@ -9,14 +8,3 @@ def check_junction():
 
 
 check_junction()
-
-# log.basicConfig(level=log.DEBUG,
-#                     format='%(levelname)s - %(message)s',
-#                     filename='app.log',
-#                     filemode='w')
-#
-# log.debug('This is a debug message')
-# log.info('This is an info message')
-# log.warning('This is a warning message')
-# log.error('This is an error message')
-# log.critical('This is a critical message')
